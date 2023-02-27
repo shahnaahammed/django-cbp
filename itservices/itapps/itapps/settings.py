ceb05ec21c62c6b0baf40bb03de918cb6c711b3a
rascove <rascove@yahoo.com>
2023-02-27 15:45:53 +0000

    Completed the system, ready to deploy to Azure App Services

--- itservices/itapps/itapps/settings.py
+++ itservices/itapps/itapps/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,13 +32,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'itreporting.apps.ItreportingConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'itreporting.apps.ItreportingConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +121,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL= 'itreporting-home'
+
+LOGIN_URL = 'login'
--- itservices/itapps/itreporting/views.py
+++ itservices/itapps/itreporting/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'itreporting/home.html')
+    return render(request, 'itreporting/home.html', {'title': 'Welcome Page'})
 
 def about(request):
-    return render(request, 'itreporting/about.html')
+    return render(request, 'itreporting/about.html', {'title': 'About Us'})
 
 def contact(request):
+    return render(request, 'itreporting/contact.html', {'title': 'Contact Us'})
+
+def report(request):
     return render(request, 'itreporting/contact.html')
