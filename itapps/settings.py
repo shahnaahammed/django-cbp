90e7c814adab837d1da8d049d6ca82be6c8d4a13
rascove <rascove@yahoo.com>
2023-03-02 11:46:28 +0000

    Rectify settings.py

--- itapps/settings.py
+++ itapps/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_SITE_NAME']]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 
 
 # Application definition
